85591d2e3e04e14d9a92f25a59d72329d80ab5d5
Alessandro da Silva Oliveira - AOl <alessandro.oliveira@iteris.com.br>
2022-10-28 14:21:15 -0300

    Validação customizada: não pode inserir email já existente ao inserir cliente

--- src/main/java/com/alessandro/sistemaloja/repository/ClienteRepository.java
+++ src/main/java/com/alessandro/sistemaloja/repository/ClienteRepository.java
@@ -3,7 +3,11 @@ package com.alessandro.sistemaloja.repository;
 import com.alessandro.sistemaloja.domain.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    @Transactional(readOnly = true)
+    Cliente findByEmail(String email);
 }
--- src/main/java/com/alessandro/sistemaloja/service/validation/ClienteInsertValidator.java
+++ src/main/java/com/alessandro/sistemaloja/service/validation/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package com.alessandro.sistemaloja.service.validation;
 
+import com.alessandro.sistemaloja.domain.Cliente;
 import com.alessandro.sistemaloja.domain.enums.TipoCliente;
 import com.alessandro.sistemaloja.dto.ClienteCreateRequest;
+import com.alessandro.sistemaloja.repository.ClienteRepository;
 import com.alessandro.sistemaloja.resources.exception.FieldMessage;
 import com.alessandro.sistemaloja.service.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -11,6 +14,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteCreateRequest> {
+
+    @Autowired
+    private ClienteRepository repository;
 
     @Override
     public void initialize(ClienteInsert constraintAnnotation) {}
@@ -25,6 +31,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
         if (createRequest.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(createRequest.getCpfOuCnpj())) {
             list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+        }
+
+        Cliente aux = repository.findByEmail(createRequest.getEmail());
+        if (aux != null) {
+            list.add(new FieldMessage("email", "Email já existente"));
         }
 
         for (FieldMessage e : list) {
