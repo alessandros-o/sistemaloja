657edc32bedeb6a931996103ee14a0cf3b0bf85b
Alessandro da Silva Oliveira - AOl <alessandro.oliveira@iteris.com.br>
2022-11-08 13:41:26 -0300

    Criando bean para database

--- src/main/java/com/alessandro/sistemaloja/config/TestConfig.java
+++ src/main/java/com/alessandro/sistemaloja/config/TestConfig.java
@@ -1,13 +1,26 @@
 package com.alessandro.sistemaloja.config;
 
+import com.alessandro.sistemaloja.service.DBService;
 import com.alessandro.sistemaloja.service.EmailService;
 import com.alessandro.sistemaloja.service.MockEmailService;
 import com.alessandro.sistemaloja.service.SmtpEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.ParseException;
+
 @Configuration
 public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public boolean instantiateDataBase() throws ParseException {
+        dbService.instantiateTestDataBase();
+        return true;
+    }
 
     @Bean
     public EmailService emailService() {
